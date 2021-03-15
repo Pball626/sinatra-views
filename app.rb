123dc5706138aadb4d7364b373f70dfee6d89edd
joey <pball626@gmail.com>
2021-03-15 14:06:37 -0500

    commit tree fe013a85a8e12b67419d975c59774b21e67ac13f

--- app.rb
+++ app.rb
@@ -2,7 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+	# get '/hello' do
+	# 	"<h1>Hello Poop</h1>"
+	# end
+
 	get '/' do
-		
+		erb :index
 	end
+
+	get "/info" do
+		erb :dogs
+	end
+
 end
